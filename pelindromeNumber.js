81622b29dc8da4e39f585c1d2e8093bf2fde6998
rushichate <rushi.chate8006@gmail.com>
2022-11-02 12:16:23 +0530

    new code

---
+++ pelindromeNumber.js
@@ -0,0 +1,14 @@
+function palindrom(str){
+let bag="";
+	for(let i =str.length; i<0; i--){
+	
+         bag=str;
+    }
+	}
+	if(bag!=str){
+		console.log(str, "is a pelindrome ");
+	}
+          else{
+		console.log(str, "is not a pelindrome ");
+	}
+}

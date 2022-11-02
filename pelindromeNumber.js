2fdfb489778b19a2ee719edb494526c130b6d097
Umesh <yomyouronlinemart@gmail.com>
2022-11-02 12:35:20 +0530

    code fixed

--- pelindromeNumber.js
+++ pelindromeNumber.js
@@ -1,14 +1,11 @@
-function palindrom(str){
+let str="naman";
 let bag="";
-	for(let i =str.length; i<0; i--){
-	
-         bag=str;
+	for(let i =str.length-1; i>=0; i--){
+        bag+=str[i];
     }
-	}
-	if(bag!=str){
+	if(bag==str){
 		console.log(str, "is a pelindrome ");
 	}
-          else{
+    else{
 		console.log(str, "is not a pelindrome ");
 	}
-}
--- primeNumber.js
+++ primeNumber.js
@@ -1,13 +1,13 @@
-let num = 12;
-let count = 0;
+let number = 12;
+let factor = 0;
 	for(let i =1; i<=num; i++){
-		if(num%i==0){
-		count++;
-			}
+		if(number%i==0){
+			factor++;
 		}
-	if(count=0){
-		console.log(num, "is a prime number");
+	}
+	if(count==2){
+		console.log(number, "is a prime number");
 	}
         else{
-		console.log(num, "is not a prime number");
+		console.log(number, "is not a prime number");
   }

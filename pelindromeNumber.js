18c215e96143bbb5f5be716d2a9a642295c8b4f4
rushichate <rushi.chate8006@gmail.com>
2022-11-02 16:14:45 +0530

    code_fixed_again

--- pelindromeNumber.js
+++ pelindromeNumber.js
@@ -1,14 +1,15 @@
 function palindrom(str){
 let bag="";
-	for(let i =str.length; i<0; i--){
+	for(let i=str.length-1; i<=0; i--){
 	
-         bag=str;
+         bag=str[i];
     }
-	}
-	if(bag!=str){
+	
+	if(bag==str){
 		console.log(str, "is a pelindrome ");
 	}
           else{
 		console.log(str, "is not a pelindrome ");
 	}
 }
+palindrom("naman");
--- primeNumber.js
+++ primeNumber.js
@@ -1,11 +1,11 @@
 let num = 12;
 let count = 0;
-	for(let i =1; i<=num; i++){
+	for(let i=2; i<=num**(1/2); i++){
 		if(num%i==0){
 		count++;
 			}
 		}
-	if(count=0){
+	if(count==0){
 		console.log(num, "is a prime number");
 	}
         else{
